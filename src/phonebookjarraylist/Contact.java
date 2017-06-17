bda363711c2cfb8f64e122d7900762f506e0070c
mhcrnl <mhcrnl@gmail.com>
2017-06-17 16:34:39 +0300

    17/06/2107

---
+++ src/phonebookjarraylist/Contact.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebookjarraylist;
+
+/**
+ *
+ * @author mhcrnl
+ */
+public class Contact {
+    
+    private int nrCrt;
+    private String nume;
+    private String prenume;
+    private String telefon;
+    private String email;
+    private String companie;
+    
+    public Contact(String nume, String prenume, String telefon, String email, String companie){
+        this.nume = nume;
+        this.prenume = prenume;
+        this.telefon = telefon;
+        this.email = email;
+        this.companie = companie;
+        this.nrCrt++;
+    } 
+    
+    @Override
+    public String toString(){
+        String contact = nrCrt+" "+nume+" "+prenume+" "+telefon+" "+email+" "+companie;
+        return contact;
+    }
+
+    /**
+     * @return the nrCrt
+     */
+    public int getNrCrt() {
+        return nrCrt;
+    }
+
+    /**
+     * @param nrCrt the nrCrt to set
+     */
+    public void setNrCrt(int nrCrt) {
+        this.nrCrt = nrCrt;
+    }
+
+    /**
+     * @return the nume
+     */
+    public String getNume() {
+        return nume;
+    }
+
+    /**
+     * @param nume the nume to set
+     */
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    /**
+     * @return the prenume
+     */
+    public String getPrenume() {
+        return prenume;
+    }
+
+    /**
+     * @param prenume the prenume to set
+     */
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    /**
+     * @return the telefon
+     */
+    public String getTelefon() {
+        return telefon;
+    }
+
+    /**
+     * @param telefon the telefon to set
+     */
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the companie
+     */
+    public String getCompanie() {
+        return companie;
+    }
+
+    /**
+     * @param companie the companie to set
+     */
+    public void setCompanie(String companie) {
+        this.companie = companie;
+    }
+}
---
+++ src/phonebookjarraylist/Controler.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonebookjarraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author mhcrnl
+ */
+public class Controler {
+    
+    ArrayList <Contact> listaContacte = new ArrayList <>();
+    String file = "contacte.txt";
+    String nume, prenume, telefon, email, companie;
+    Contact contact;
+    Scanner in;
+    
+    public void adaugaContact(){
+        in = new Scanner(System.in);
+        System.out.println("Introduceti numele contactului: ");
+        nume = in.next();
+        System.out.println("Introduceti prenumele contactului: ");
+        prenume = in.next();
+        System.out.println("Introduceti numarul de telefon; ");
+        telefon = in.next();
+        System.out.println("Introduceti adresa de email: ");
+        email = in.next();
+        System.out.println("Introduceti numele companiei: ");
+        companie = in.next();
+        
+        contact = new Contact(nume, prenume, telefon, email, companie);
+        listaContacte.add(contact);
+        
+        
+    }
+    
+    public void afiseazaContact(){
+        
+    }
+    
+    public void meniu(){
+        
+        int select;
+        //Controler controller = new Controler();
+        in = new Scanner(System.in);
+        do{
+            // Meniul aplicatiei
+            System.out.println("=============== <> MENIU <> =================");
+            System.out.println("============<> 1 ADAUGA COMTACT  ============");
+            System.out.println("============<> 2 AFISEAZA CONTACTE ==========");
+            System.out.println("============<> 4 INCHIDE APLICATIA ==========");
+            System.out.println("Selectati o optiune: ");
+            select = in.nextInt();
+            int number =1;
+            switch(select){
+                case 1:
+                    adaugaContact();
+                    
+                    break;
+                case 2:
+                    afiseazaContact();
+                    
+                    break;
+                case 4:
+                        
+            }
+        } while (select !=4);
+        
+        
+    }
+}
